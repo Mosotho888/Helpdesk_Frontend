b05146ebc8592ed905a16846c29319f40c0178b1
Tebogo Mofokeng <tebohogivenmofokeng@gmail.com>
2026-07-29 21:36:25 +0200

    fix: resolve CI lint failures (disable false-positive rules on shadcn-generated ui files)

--- eslint.config.js
+++ eslint.config.js
@@ -25,7 +25,7 @@ export default defineConfig([
     files: ['src/components/ui/**/*.{ts,tsx}'],
     rules: {
       'react-refresh/only-export-components': 'off',
-      'jsx-a11y/label-has-associated-control': 'off',
+      'jsx-a11y-x/label-has-associated-control': 'off',
     },
   },
 ])
